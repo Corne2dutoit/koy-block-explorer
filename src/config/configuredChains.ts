cbb13cdac51202de77cf655e793ff5a655211f12
Corne2dutoit <corne@kingkong.co.za>
2024-02-29 14:17:19 +0200

    Added koy-testnet

--- src/config/configuredChains.ts
+++ src/config/configuredChains.ts
@@ -7,10 +7,11 @@ import TelosTestnet from 'src/config/chains/telos-testnet';
 import Jungle from 'src/config/chains/jungle';
 
 import { ChainsConfig } from 'src/types/ChainsConfig';
+import KoyTestnet from 'src/config/chains/koy-testnet';
 
 const chains: ChainsConfig = {
     mainnets: [new EOS('eos'), new Telos('telos'), new UX('ux'), new Wax('wax')],
-    testnets: [new Jungle('jungle'), new TelosTestnet('telos-testnet')],
+    testnets: [new Jungle('jungle'), new TelosTestnet('telos-testnet'), new KoyTestnet('koy-testnet')],
 };
 
 export default chains;
