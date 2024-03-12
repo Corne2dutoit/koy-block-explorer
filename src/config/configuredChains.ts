3e2653351c70bf1a7f4f52624dc14d81381c2e9d
Corne2dutoit <corne@kingkong.co.za>
2024-03-12 10:48:02 +0200

    Added koy mainnet and removed other chains

--- src/config/configuredChains.ts
+++ src/config/configuredChains.ts
@@ -1,17 +1,20 @@
-import EOS from 'src/config/chains/eos';
-import Telos from 'src/config/chains/telos';
-import UX from 'src/config/chains/ux';
-import Wax from 'src/config/chains/wax';
+// import EOS from 'src/config/chains/eos';
+// import Telos from 'src/config/chains/telos';
+// import UX from 'src/config/chains/ux';
+// import Wax from 'src/config/chains/wax';
 
-import TelosTestnet from 'src/config/chains/telos-testnet';
-import Jungle from 'src/config/chains/jungle';
+// import TelosTestnet from 'src/config/chains/telos-testnet';
+// import Jungle from 'src/config/chains/jungle';
 
 import { ChainsConfig } from 'src/types/ChainsConfig';
 import KoyTestnet from 'src/config/chains/koy-testnet';
+import Koy from 'src/config/chains/koy';
 
 const chains: ChainsConfig = {
-    mainnets: [new EOS('eos'), new Telos('telos'), new UX('ux'), new Wax('wax')],
-    testnets: [new Jungle('jungle'), new TelosTestnet('telos-testnet'), new KoyTestnet('koy-testnet')],
+    // mainnets: [new EOS('eos'), new Telos('telos'), new UX('ux'), new Wax('wax'), new Koy('koy')],
+    mainnets: [new Koy('koy')],
+    // testnets: [new Jungle('jungle'), new TelosTestnet('telos-testnet'), new KoyTestnet('koy-testnet')],
+    testnets: [new KoyTestnet('koy-testnet')],
 };
 
 export default chains;
